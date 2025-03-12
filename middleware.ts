fefc70eb553f6f3f46d451ace8f3df00d6a42418
Kamran <155973381+Kamrancodex@users.noreply.github.com>
2025-03-12 15:11:37 +0530

    loging errors2

--- middleware.ts
+++ middleware.ts
@@ -26,6 +26,7 @@ export async function middleware(request: NextRequest) {
   if (!token && protectedPaths.some((p) => p.test(pathname))) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
+  console.log("All cookies:", request.cookies.getAll());
 
   // Check for session cart cookie
   if (!request.cookies.get("sessionCartId")) {
