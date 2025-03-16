8389c095051cc3b12b1e7daa20659ae85408d5b3
Kamran <155973381+Kamrancodex@users.noreply.github.com>
2025-03-16 20:10:51 +0530

    search func in dashboard

--- lib/validators.ts
+++ lib/validators.ts
@@ -127,3 +127,8 @@ export const updateProfileSchema = z.object({
   name: z.string().min(3, "Name mush be at least 3 characters"),
   email: z.string().min(3, "Email mush be at least 3 characters"),
 });
+// Schema to update users
+export const updateUserSchema = updateProfileSchema.extend({
+  id: z.string().min(1, "ID is required"),
+  role: z.string().min(1, "Role is required"),
+});
