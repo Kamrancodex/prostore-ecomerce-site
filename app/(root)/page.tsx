da41b20faff7963cf491544ec50c8c397d4d4f8c
Kamran <155973381+Kamrancodex@users.noreply.github.com>
2025-03-17 14:10:38 +0530

    added add review filters on products and also featured prod section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 
 const HomePage = async () => {
   const latestProduct = await getLatestProducts();
-  return <ProductList data={latestProduct} title="New Arrival" limit={4} />;
+  const featuredProducts = await getFeaturedProducts();
+
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProduct} title="New Arrival" limit={4} />;
+      <ViewAllProductsButton />
+    </>
+  );
 };
 export default HomePage;
--- app/(root)/products/[slug]/page.tsx
+++ app/(root)/products/[slug]/page.tsx
@@ -1,38 +1,45 @@
 import { Badge } from "@/components/ui/badge";
-
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
-import AddToCart from "./add-to-cart";
+import AddToCart from "@/components/shared/product/add-to-cart";
 import { getMyCart } from "@/lib/actions/cart.actions";
+import ReviewList from "./review-list";
+import { auth } from "@/auth";
+import Rating from "@/components/shared/product/rating";
+
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
 }) => {
   const { slug } = await props.params;
+
   const product = await getProductBySlug(slug);
   if (!product) notFound();
+
+  const session = await auth();
+  const userId = session?.user?.id;
+
   const cart = await getMyCart();
+
   return (
     <>
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
-          {/*image column */}
+          {/* Images Column */}
           <div className="col-span-2">
-            {/* images component*/}
             <ProductImages images={product.images} />
           </div>
-          {/*Details column */}
+          {/* Details Column */}
           <div className="col-span-2 p-5">
-            <div className="flex flex-col gsp-6">
+            <div className="flex flex-col gap-6">
               <p>
                 {product.brand} {product.category}
               </p>
               <h1 className="h3-bold">{product.name}</h1>
-              <p>
-                {product.rating} of {product.numReviews} Reviews
-              </p>
+              <Rating value={Number(product.rating)} />
+              <p>{product.numReviews} reviews</p>
               <div className="flex flex-col sm:flex-row sm:items-center gap-3">
                 <ProductPrice
                   value={Number(product.price)}
@@ -45,7 +52,7 @@ const ProductDetailsPage = async (props: {
               <p>{product.description}</p>
             </div>
           </div>
-          {/* action col*/}
+          {/* Action Column */}
           <div>
             <Card>
               <CardContent className="p-4">
@@ -60,7 +67,7 @@ const ProductDetailsPage = async (props: {
                   {product.stock > 0 ? (
                     <Badge variant="outline">In Stock</Badge>
                   ) : (
-                    <Badge variant="destructive">Out of Stock</Badge>
+                    <Badge variant="destructive">Out Of Stock</Badge>
                   )}
                 </div>
                 {product.stock > 0 && (
@@ -83,7 +90,16 @@ const ProductDetailsPage = async (props: {
           </div>
         </div>
       </section>
+      <section className="mt-10">
+        <h2 className="h2-bold mb-5">Customer Reviews</h2>
+        <ReviewList
+          userId={userId || ""}
+          productId={product.id}
+          productSlug={product.slug}
+        />
+      </section>
     </>
   );
 };
+
 export default ProductDetailsPage;
